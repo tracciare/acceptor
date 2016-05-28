9e2ab7c282dc36f5901c8b434cb1d683282b52bd
fiorenzo pizza <fiorenzino@gmail.com>
2016-05-28 22:14:08 +0100

    simple test

--- src/test/java/re/traccia/MyRestIT.java
+++ src/test/java/re/traccia/MyRestIT.java
@@ -1,12 +1,23 @@
 package re.traccia;
 
 import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import io.vertx.core.json.Json;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import re.traccia.management.AppConstants;
+import re.traccia.model.Trace;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static com.jayway.restassured.RestAssured.get;
+import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.RestAssured.post;
+import static com.jayway.restassured.parsing.Parser.JSON;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 /**
@@ -16,7 +27,7 @@ public class MyRestIT {
 
     @BeforeClass
     public static void configureRestAssured() {
-        RestAssured.baseURI = "http://localhost";
+        RestAssured.baseURI = "http://192.168.99.100";
         RestAssured.port = Integer.getInteger("http.port", 8080);
     }
 
@@ -26,7 +37,7 @@ public class MyRestIT {
     }
 
     @Test
-    public void getList() {
+    public void getListTest() {
         // Get the list of bottles, ensure it's a success and extract the first id.
         final int id = get(AppConstants.TRACES_PATH).then()
                 .assertThat()
@@ -40,6 +51,22 @@ public class MyRestIT {
                 .body("name", equalTo("Bowmore 15 Years Laimrig"))
                 .body("origin", equalTo("Scotland, Islay"))
                 .body("id", equalTo(id));
+    }
+
+
+    @Test
+    public void postTest() throws IOException {
+        Path path = Paths.get("docs/auto.jpg");
+        byte[] data = Files.readAllBytes(path);
+        Trace trace = new Trace("lat", "lon", data, null);
+        given()
+                .contentType(ContentType.JSON)
+                .body(Json.encode(trace))
+                .when()
+                .post(AppConstants.TRACES_PATH)
+                .then()
+                .assertThat()
+                .statusCode(200);
     }
 
 
