25f049457b9526d32e0a665c68b9bb78b1032e7a
fiorenzo pizza <fiorenzino@gmail.com>
2016-05-28 21:57:49 +0100

    simple test using RestAssured

---
+++ src/test/java/re/traccia/MyRestIT.java
@@ -0,0 +1,43 @@
+package re.traccia;
+
+import com.jayway.restassured.RestAssured;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static com.jayway.restassured.RestAssured.get;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+/**
+ * Created by fiorenzo on 28/05/16.
+ */
+public class MyRestIT {
+
+    @BeforeClass
+    public static void configureRestAssured() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = Integer.getInteger("http.port", 8080);
+    }
+
+    @AfterClass
+    public static void unconfigureRestAssured() {
+        RestAssured.reset();
+    }
+
+    @Test
+    public void checkThatWeCanRetrieveIndividualProduct() {
+        // Get the list of bottles, ensure it's a success and extract the first id.
+        final int id = get("/api/traces").then()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .jsonPath().getInt("find { it.name=='Bowmore 15 Years Laimrig' }.id");
+        // Now get the individual resource and check the content
+        get("/api/whiskies/" + id).then()
+                .assertThat()
+                .statusCode(200)
+                .body("name", equalTo("Bowmore 15 Years Laimrig"))
+                .body("origin", equalTo("Scotland, Islay"))
+                .body("id", equalTo(id));
+    }
+}
