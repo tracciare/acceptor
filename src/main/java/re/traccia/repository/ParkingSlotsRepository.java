b2f27af61c51631ad8242d3b13f55699e09df6bd
Fabio Cognigni <fabio.cognigni@gmail.com>
2016-05-29 23:35:15 +0100

    MongoClient date range query example

--- src/main/java/re/traccia/repository/ParkingSlotsRepository.java
+++ src/main/java/re/traccia/repository/ParkingSlotsRepository.java
@@ -2,10 +2,15 @@ package re.traccia.repository;
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.mongo.MongoClient;
 import re.traccia.common.AbstractRepository;
 import re.traccia.management.AppConstants;
+
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by fiorenzo on 28/05/16.
@@ -20,6 +25,19 @@ public class ParkingSlotsRepository extends AbstractRepository {
     public void findByPlatenumber(String plateNumber, Handler<AsyncResult<JsonObject>> handler) {
         JsonObject jsonObject = new JsonObject().put("plateNumber", plateNumber);
         getMongoClient().findOne(AppConstants.PARKINGSLOTS, jsonObject, null, handler);
+    }
+
+    public void platenumberInCurrentSlot(String plateNumber, Handler<AsyncResult<Long>> handler) {
+        Instant now = Instant.now();
+
+        JsonObject plateNumberQuery = new JsonObject().put("plateNumber", plateNumber);
+        JsonObject startDateLtNowQuery = new JsonObject().put("startDate", new JsonObject().put("$lt", new JsonObject().put("$date", now)));
+        JsonObject endDateGtNowQuery = new JsonObject().put("endDate", new JsonObject().put("$gt", new JsonObject().put("$date", now)));
+        JsonObject startAndEnd = new JsonObject().put("$and", new JsonArray(Arrays.asList(startDateLtNowQuery, endDateGtNowQuery)));
+        JsonObject query = new JsonObject().put("$and", new JsonArray(Arrays.asList(plateNumberQuery, startAndEnd)));
+
+
+        getMongoClient().count(AppConstants.PARKINGSLOTS, query, handler);
     }
 
 
---
+++ src/test/java/re/traccia/re/traccia/repository/TraceRespositoryTest.java
@@ -0,0 +1,47 @@
+package re.traccia.re.traccia.repository;
+
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.ext.mongo.MongoClient;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import re.traccia.model.Trace;
+import re.traccia.repository.TracesRepository;
+import re.traccia.verticle.MainVerticle;
+
+import java.time.Instant;
+
+@RunWith(VertxUnitRunner.class)
+public class TraceRespositoryTest {
+
+    Vertx vertx;
+    MongoClient mongoClient;
+    TracesRepository repo;
+
+    @Before
+    public void init() {
+        vertx = Vertx.vertx();
+        mongoClient = MongoClient.createNonShared(vertx, MainVerticle.mongoConfig());
+        repo = new TracesRepository(mongoClient);
+    }
+
+
+    @Test
+    public void saveTrace(TestContext context) {
+        final Async async = context.async();
+
+        Trace trace = new Trace("lat", "lon", null, "12345");
+        trace.setStartDate(Instant.now());
+        trace.setEndDate(Instant.now());
+
+        repo.create(trace.toJson(), result -> {
+            repo.delete(result.result(), resultDel -> {});
+            async.complete();
+        });
+    }
+
+}
